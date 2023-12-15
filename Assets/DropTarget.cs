d8acdc2228db89c64ed7125d2c9a8b9adba4bb3f
SaphyChaos <kit.heckman@gmail.com>
2023-12-15 12:50:44 -0500

    potion now contains ingredients

--- Assets/DropTarget.cs
+++ Assets/DropTarget.cs
@@ -2,6 +2,7 @@ using PixelCrushers.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class DropTarget : MonoBehaviour, IDropHandler//drop target handles all the values for ingrediants. Think of droptarget as a feature of Potion. It's literally what reads the ingrediants and contributes to potion values accordingly.
@@ -9,20 +10,65 @@ public class DropTarget : MonoBehaviour, IDropHandler//drop target handles all t
     public string DroppedItem = "empty";
     public GameObject UIManager;
     public GameObject Potion;
+    public GameObject Slot1;
+    public GameObject Slot2;
+    public GameObject Slot3;
+    public bool Slot1Filled;
+    public bool Slot2Filled;
+    public bool Slot3Filled;
     //public GameObject DialogueOption1;
     //public GameObject DialogueOption2;
     //public GameObject DialogueOption3;
     //public GameObject DialogueOption4;
+    public void PotionReset()
+    {
+        Slot1Filled = false;
+        Slot2Filled = false;
+        Slot3Filled = false;
+        Slot1 = null;
+        Slot2 = null;
+        Slot3 = null;
+    }
+    private void SlotFiller(string objectName)
+    {
+        var myObject = GameObject.Find(objectName);//naming shit properly is important!
+        if(myObject == null)
+        {
+            Debug.Log("that object couldn't be found even though you just dropped it??");
+            return;
+        }
+        if (Slot1Filled)
+        {
+            if (Slot2Filled)
+            {
+                Slot3Filled = true;
+                Slot3.GetComponent<Image>().sprite = myObject.GetComponent<Image>().sprite;
+                return;
+            }
+            Slot2Filled = true;
+            Slot2.GetComponent<Image>().sprite = myObject.GetComponent<Image>().sprite;
+        }
+        else
+        {
+            Slot1Filled = true;
+            Slot1.GetComponent<Image>().sprite = myObject.GetComponent<Image>().sprite;
+        }
+    }
     public void OnDrop(PointerEventData eventData)
     {
+        if (Slot3Filled)
+        {
+            return;
+        }
         GameObject dropped = eventData.pointerDrag;
         DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
         DroppedItem = draggableItem.name;
+
         //Debug.Log("oh!"+draggableItem.name);
         //UIManager.GetComponent<UIUpdater>().moveIngredientsOut();
+        SlotFiller(DroppedItem);
         if (DroppedItem == "Dick")
         {
-            print("oh dick");
             Potion.GetComponent<PotionScript>().TreatsDepressedVal += 25;
             Potion.GetComponent<PotionScript>().TreatsAssholeChappedVal -= 50;
         }
